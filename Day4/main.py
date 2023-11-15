58af5ce03e7e02f16ec84f64a4a4425f6a47b374
KostasEreksonas <k.ereksonas@gmail.com>
2023-11-15 11:40:06 +0200

    Day 4

---
+++ Day4/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+print(my_module.pi)
+
+random_float = random.random()
+print(random_float)
